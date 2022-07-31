aca77f564cc6e04ba3809a4e172f8dfa517c69a9
Shoaib Ali Ajaib <shoaibaliajaib@Shoaibs-MacBook-Pro.local>
2022-07-31 15:13:01 +0100

    Updated Install.R file

--- install.R
+++ install.R
@@ -1,14 +1,14 @@
-#' install.R for Gliovis
+#' install.R for GBMDeconvoluteR
 
-# This file is part of GlioVis
-# Copyright (C) Massimo Squatrito
+# This file is part of GMBDeconvoluteR
+# Copyright (C) Shoaib Ali Ajaib
 #
-# GlioVis is free software; you can redistribute it and/or modify it under the
+# GMBDeconvoluteR is free software; you can redistribute it and/or modify it under the
 # terms of the GNU General Public License as published by the Free Software
 # Foundation; either version 3 of the License, or (at your option) any later
 # version.
 # 
-# GlioVis is distributed in the hope that it will be useful, but WITHOUT ANY
+# GMBDeconvoluteR is distributed in the hope that it will be useful, but WITHOUT ANY
 # WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
 # A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 # 
@@ -16,23 +16,25 @@
 # this program; if not, see <http://www.gnu.org/licenses/>.
 
 #######################################################
-# gliovis Installation Instructions: 
-# see https://github.com/stan-dev/shinystan/blob/develop/install_shinystan.R
+# GMBDeconvoluteR Installation Instructions: 
 
 # 1) Restart R
-# 2) Run install_gliovis(), included below, to install
+# 2) Run install_GMBDeconvoluteR(), included below, to install
 
 #######################################################
 
-install_gliovis <- function() {
+install_GBMDeconvoluteR <- function() {
   
-  if (getRversion() < '4.0.0') stop("GlioVis requires R version 4.0.0 or greater.")
+  if (getRversion() < '4.0.0') stop("GBMDeconvoluteR requires R version 4.0.0 or greater.")
   
-  msg <- "Note: this will install or update packages needed to run GlioVis Do you want to continue?"
+  
+  msg <- "Note: this will install or update packages needed to run GBMDeconvoluteR Do you want to continue?"
+  
   
   continue <- select.list(choices = c("Yes", "No"), 
                           title = msg, 
                           graphics = FALSE)
+  
   
   if (continue == "No") {
     message("Installation canceled by user.")
@@ -40,9 +42,8 @@ install_gliovis <- function() {
   }
   
   # install needed packages from CRAN
-  pkg <- c("shiny", "survival", "weights", "googleVis", "tidyverse","shinydashboard", 
-           "htmlwidgets","kernlab","devtools","markdown","caret","Cairo", "DT","GGally",
-           "Cairo","reshape2", "cgdsr","shinyBS", "gridExtra","survminer","plotly")
+  pkg <- c("shiny", "shinyBS", "tidyverse", "DT", "openxlsx",
+           "devtools","curl")
   
   new.pkg <- pkg[!(pkg %in% installed.packages())]
   
@@ -53,18 +54,22 @@ install_gliovis <- function() {
   update.packages(pkg[!(pkg %in% new.pkg)])
 
   # install needed packages from Bioconductor
-  bioc <- c("GSVA","limma","DOSE","clusterProfiler")
-  if(!(bioc %in% installed.packages())){
-    source("http://bioconductor.org/biocLite.R")
-    biocLite(bioc) 
-  }
+  # bioc <- c()
+  # 
+  # if(!(bioc %in% installed.packages())){
+  #   source("http://bioconductor.org/biocLite.R")
+  #   BiocManager::install(pkgs = bioc)
+  # }
 
   # install needed packages from Github
-  devtools::install_github("jokergoo/ComplexHeatmap")
-  # devtools::install_github(c("GuangchuangYu/DOSE", "GuangchuangYu/clusterProfiler"))
   
-  message("\n All set. \n You might need to restart R before using GlioVis \n")
+  devtools::install_github("ajxa/GBMDeconvoluteR")
+  devtools::install_github("ebecht/MCPcounter")
+  
+
+  message("\n All set. \n You might need to restart R before using GBMDeconvoluteR \n")
+  
   return(invisible(NULL))
 }
 
-install_gliovis()
+install_GBMDeconvoluteR()
